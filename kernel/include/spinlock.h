309a389a5610d6f6fc92d97ff5aa888bef2c67fd
wcyiming <wcyiming@outlook.com>
2022-04-06 01:49:12 +0800

    init lock unlock

--- kernel/include/spinlock.h
+++ kernel/include/spinlock.h
@@ -3,12 +3,16 @@
 
 #include <kernel.h>
 
-struct spinlock {
-  /* data */
+struct spinlock_t {
+  bool lock_flag;     //check the lock locked , 1 means locked
+  const char *name;   //Name of lock
+  int hold_cpuid;     //The cpu holding the lock
+  int pcs[10];        //The call stack that locked the lock(for debugger)
 };
 
 void spin_init(spinlock_t *lk, const char *name);
 void spin_lock(spinlock_t *lk);
 void spin_unlock(spinlock_t *lk);
+
 
 #endif
--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -1,10 +1,31 @@
 #include <spinlock.h>
 
 void spin_init(spinlock_t *lk, const char *name) {
+  lk->lock_flag = 0;
+  lk->name = name;
+  lk->hold_cpuid = -1;
 }
 
 void spin_lock(spinlock_t *lk) {
+  //spin_pushcli();     //interrupt disable
+  //Assert();
+
+  while(atomic_xchg((intptr_t *) &lk->lock_flag, 1)) {
+    ;
+  }
+  //warning: this func unable before gdb version year 2008
+  //to force the CPU to operate the after code after the before code
+  __sync_synchronize(); //memory barrier
+
+  lk->hold_cpuid = cpu_current();
+
 }
 
 void spin_unlock(spinlock_t *lk) {
+  //Assert();
+  lk->hold_cpuid = -1;
+  __sync_synchronize(); //memory barrier
+  
+  asm volatile ("movl $0, %0" : "+m"(lk->lock_flag) : );
+  //spin_popcli();
 }
