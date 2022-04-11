18ca3c9428504dfbe260922d18ef5fb00f6ce8de
moeakwak <moeakwak@gmail.com>
2022-04-11 23:50:24 +0800

    feat: test framework update (BREAKING CHANGE)

--- test/units/kernel_example.c
+++ test/units/kernel_example.c
@@ -1,12 +1,20 @@
 #include <kernel.h>
 #include <klib.h>
+#include <logger.h>
+#include <spinlock.h>
+
+spinlock_t lock;
 
 static void run() {
-  printf("Hello World from CPU #%d\n", cpu_current());
-  halt(0);
+  spin_lock(&lock);
+  info("Hello World from CPU #%d\n", cpu_current());
+  spin_unlock(&lock);
+  while (1)
+    ;
 }
 
 int main() {
-  os->init();
+  ioe_init();
+  spin_init(&lock, "lock");
   mpe_init(&run);
 }
