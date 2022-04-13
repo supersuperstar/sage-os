270cb6b442e3c6df2b872ed2e4ed06494b6e7297
moeakwak <80299225+moeakwak@users.noreply.github.com>
2022-04-14 03:58:02 +0800

    Revert "dev-2-kmt: logger allow change log_mask; add assert_msg"

--- test/units/thread-print.c
+++ test/units/thread-print.c
@@ -5,50 +5,34 @@
 #include <thread.h>
 #include <logger.h>
 
-#define MAX_TASK      8
+#define MAX_TASK      4
 #define RUNNING_COUNT 3
 
 task_t tasks[MAX_TASK];
 char names[10][MAX_TASK];
 int ids[MAX_TASK];
 
-int cnt = 0;  // current running thread count
-spinlock_t cnt_lock, print_lock;
+spinlock_t print_lock;
 
 void func(void *arg) {
   int id = *(int *)arg;
-
-  spin_lock(&cnt_lock);
-  cnt++;
-  spin_unlock(&cnt_lock);
-
   for (int i = 0; i < RUNNING_COUNT; i++) {
-    // print something
+    // every thread run once, and then yield
     spin_lock(&print_lock);
-    warn("T%02d running %d times", id, i + 1);
+    info("T%03d running %03d times", id, i + 1);
     kmt_print_cpu_tasks();
     printf("\n");
     spin_unlock(&print_lock);
-    // go to kernel; current cpu re-schedule task
     yield();
   }
-
-  // if finished
   spin_lock(&print_lock);
-  warn("T%03d exit!", id);
+  success("T%03d exit!", id);
   kmt_print_all_tasks();
   kmt_print_cpu_tasks();
   spin_unlock(&print_lock);
-
-  // hold cpu
-  int remain = 0;
-  spin_lock(&cnt_lock);
-  remain = --cnt;
-  spin_unlock(&cnt_lock);
-  if (remain < cpu_count())
+  if (id < cpu_count())
     while (1)
       ;
-  yield();
 }
 
 static void create_threads() {
@@ -63,10 +47,6 @@ int main() {
   ioe_init();
   cte_init(os->trap);
   os->init();
-
-  spin_init(&print_lock, "print_lock");
-  spin_init(&cnt_lock, "cnt_lock");
-
   create_threads();
   kmt_print_all_tasks();
   kmt_print_cpu_tasks();
