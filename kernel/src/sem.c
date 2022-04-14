ff32d558920efcbcbe02ecf9ceaa98732309f915
moeakwak <moeakwak@gmail.com>
2022-04-14 17:18:18 +0800

    fix: wrong lock in kmt_yield and spinlock

--- kernel/src/sem.c
+++ kernel/src/sem.c
@@ -11,18 +11,18 @@ void sem_init(sem_t *sem, const char *name, int value) {
 }
 
 void sem_wait(sem_t *sem) {
-  assert(!spin_holding(&ir_lock));
+  assert_msg(!spin_holding(&ir_lock), "do not allow sem_wait in trap");
   spin_lock(&sem->lock);
 
   while (sem->value <= 0) {
-    assert(!spin_holding(&ir_lock));
+    assert_msg(!spin_holding(&ir_lock), "do not allow sleep in trap");
 
     spin_unlock(&sem->lock);
-    spin_lock(&ir_lock);
+    spin_lock(&task_list_lock);
     task_t *cur = kmt->get_task();
     assert(cur);
     cur->wait_sem = sem;
-    spin_unlock(&ir_lock);
+    spin_unlock(&task_list_lock);
     yield();
     spin_lock(&sem->lock);
   }
@@ -37,13 +37,13 @@ void sem_signal(sem_t *sem) {
   ++sem->value;
   spin_unlock(&sem->lock);
 
-  bool holding = spin_holding(&ir_lock);
-  if (!holding) spin_lock(&ir_lock);
+  bool holding = spin_holding(&task_list_lock);
+  if (!holding) spin_lock(&task_list_lock);
   for (task_t *tp = root_task.next; tp != NULL; tp = tp->next) {
     if (tp->wait_sem == sem) {
       if (tp->state == ST_S) tp->state = ST_W;
       tp->wait_sem = NULL;  // stop going to sleep
     }
   }
-  if (!holding) spin_unlock(&ir_lock);
+  if (!holding) spin_unlock(&task_list_lock);
 }
